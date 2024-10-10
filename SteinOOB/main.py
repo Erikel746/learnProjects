be2a1a6206e5c233a030c294ebdab25e54210fbc
Erikel <erikel1612@gmail.com>
2024-10-10 12:01:16 +0200

    create second Auto

--- SteinOOB/main.py
+++ SteinOOB/main.py
@@ -3,12 +3,14 @@ from VerbrennerAuto import VerbrennerAuto
 
 def main():
     elektro_auto = ElektroAuto("Tesla", 2022, 5, 100)
-    verbrenner_auto = VerbrennerAuto("Volkswagen", 2023, 3, 50)
+    verbrenner_auto2 = VerbrennerAuto("Volkswagen", 2023, 3, 50)
+    verbrenner_auto = VerbrennerAuto("BMW", 2006, 7, 110)
 
     print("ElektroAuto Info:")
     elektro_auto.anzeigen_info()
 
     print("\nVerbrennerAuto Info:")
+    verbrenner_auto2.anzeigen_info()
     verbrenner_auto.anzeigen_info()
 
 
