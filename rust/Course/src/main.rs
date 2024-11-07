d7016c636b3c0b5c054872c087c0991328b08184
erikel746 <erikel1612@gmail.com>
2024-11-07 22:24:14 +0100

    2. Day Rust

--- rust/Course/src/main.rs
+++ rust/Course/src/main.rs
@@ -3,7 +3,15 @@ mod types;
 fn main() {
     hello_world();
     tell_number(43);
-    types::types()
+    human_id("John", 18, 132);
+
+    let _x: i32 = {
+        let price: i32 = 5;
+        let qty: i32 = 5;
+        price * qty
+    };
+    println!("Result is:{:?}", _x);
+    println!("BMI={:.2}", calculate_bmi(1.84, 80.4));
 }
 fn hello_world() {
     println!("Hello, world!");
@@ -12,3 +20,14 @@ fn hello_world() {
 fn tell_number(number: i8) {
     println!("Tell number! {}", number );
 }
+
+fn human_id(name: &str, age: i8, height: i16) {
+    println!("Human id: My name is: {}, My name is: {}, My height is: {}", name, age, height );
+
+}
+
+fn calculate_bmi(height: f32, weight: f32) -> f32 {
+
+    weight / (height * height)
+}
+
